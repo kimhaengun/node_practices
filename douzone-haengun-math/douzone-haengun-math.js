5b23ec951aa6d27e15e864bd728db5ce18c7b0d5
kimhaengun <chxl89@naver.com>
2021-11-15 13:36:02 +0900

    수정

--- douzone-haengun-math/douzone-haengun-math.js
+++ douzone-haengun-math/douzone-haengun-math.js
@@ -10,7 +10,7 @@ module.exports = {
     max:function(){
         //제일 작은 값
         var max = Number.MIN_SAFE_INTEGER;
-        Array.from(max).forEach(function(e){
+        Array.from(arguments).forEach(function(e){
             max = e > max ? e : max;
         });
         return max;
@@ -18,7 +18,7 @@ module.exports = {
     min:function(){
         //제일 작은 값
         var min = Number.MAX_SAFE_INTEGER;
-        Array.from(min).forEach(function(e){
+        Array.from(arguments).forEach(function(e){
             min = e < min ? e : min;
         });
         return min;
