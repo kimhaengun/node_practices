4d08cbd52403577ed8a0d8ae8ed8fe602a9c8702
kimhaengun <chxl89@naver.com>
2021-11-16 16:36:40 +0900

    Promise async await

--- emaillist/models/emaillist.js
+++ emaillist/models/emaillist.js
@@ -1,3 +1,11 @@
+//async 키워드를 추가하면  Promise를 반환한다. --> 반환 값은 Promise
+//await 는 async function 안에서만 사용할 수 있다.
+//awiat 키워드를 사용하면 Promise가 fulfiled되기 전 까지 다음 await를 차단
+
+//resolve 를 호출하게 된다면 (비동기)작업 성공!! 
+//reject 를 호출하게 된다면 (비동기)작업 실패!!
+
+
 const mysql = require('mysql2');
 const util = require('util');
 module.exports = {
